76f68cc15d5efd717b3ea65cbe95b9ee1f2cc39f
HangVy <nguyenhangth10a@gmail.com>
2016-03-19 21:46:45 +0700

    hhh

--- DATN1/DATN1/Program.cs
+++ DATN1/DATN1/Program.cs
@@ -16,7 +16,7 @@ namespace DATN1
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Main());
+            Application.Run(new FormatForm());
         }
     }
 }
