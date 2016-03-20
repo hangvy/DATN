63f008695b438df633ac06148e71ffca7e7d8f51
hoanganhmta <anh.th10b@gmail.com>
2016-03-20 08:35:32 +0700

    add referrence and Authentication.cs

---
+++ DATN1/DATN1/Ultilities/Authentication.cs
@@ -0,0 +1,24 @@
+﻿using Google.Apis.Drive.v2;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DATN1.Ultilities
+{
+    public class Authentication
+    {
+        /// <summary>
+        /// Authenticate to Google Using Oauth2
+        /// Documentation https://developers.google.com/accounts/docs/OAuth2
+        /// </summary>
+        /// <param name="clientId">From Google Developer console https://console.developers.google.com</param>
+        /// <param name="clientSecret">From Google Developer console https://console.developers.google.com</param>
+        /// <param name="userName">A string used to identify a user.</param>
+        /// <returns></returns>
+
+        public static DriveService AuthenticateOauth() { }
+
+    }
+}
