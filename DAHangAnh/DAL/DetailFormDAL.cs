d22026c6b02ca93f6bcf87943a193f799a232d66
HangVy <nguyenhangth10a@gmail.com>
2016-03-23 06:43:52 +0700

    add DAL/DetailFormDAL.cs and DAL/FormDAL.cs

---
+++ DAHangAnh/DAL/DetailFormDAL.cs
@@ -0,0 +1,63 @@
+﻿using DAHangAnh.DAL.Entities;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAHangAnh.DAL
+{
+    public class DetailFormDAL
+    {
+        public DataTable fnGetListDetailForm(string strConn)
+        {
+            var conn = new DBConnect(strConn);
+            try
+            {
+                var dt = new DataTable();
+                dt = conn.ExecuteReaderNonParmarter("spGetListDetailForm");
+                return dt;
+            }
+            catch (Exception ex)
+            {
+                return null;
+                throw ex;
+            }
+            finally
+            {
+                conn.CloseConnection();
+            }
+        }
+
+
+        // Inssert  a form detail
+        public bool fnInsertDetailForm(string strConn, DetailFormEntity detailFormE)
+        {
+            var conn = new DBConnect(strConn);
+            try
+            {
+                SqlParameter[] p = new SqlParameter[6];
+                p[0] = new SqlParameter("@fieldName", detailFormE.fieldName);
+                p[1] = new SqlParameter("@coordinateX", detailFormE.coordinateX);
+                p[2] = new SqlParameter("@coordinateY", detailFormE.coordinateY);
+                p[3] = new SqlParameter("@width", detailFormE.width);
+                p[4] = new SqlParameter("@height", detailFormE.height);
+                p[5] = new SqlParameter("@location", detailFormE.location);
+                p[6] = new SqlParameter("@idForm", detailFormE.idForm);
+                conn.ExecuteNonQueryParmarter("spInsertDetailForm", p);
+                return true;
+            }
+            catch (Exception ex)
+            {
+                return false;
+                throw ex;
+            }
+            finally
+            {
+                conn.CloseConnection();
+            }
+        }
+    }
+}
---
+++ DAHangAnh/DAL/FormDAL.cs
@@ -0,0 +1,82 @@
+﻿using DAHangAnh.DAL.Entities;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAHangAnh.DAL
+{
+    public class FormDAL
+    {
+        public DataTable fnGetListForm(string strConn)
+        {
+            var conn = new DBConnect(strConn);
+            try
+            {
+                var dt = new DataTable();
+                dt = conn.ExecuteReaderNonParmarter("spGetListForm");
+                return dt;
+            }
+            catch (Exception ex)
+            {
+                return null;
+                throw ex;
+            }
+            finally
+            {
+                conn.CloseConnection();
+            }
+        }
+
+
+        // Inssert  a form
+        public bool fnInsertForm(string strConn, FormEntity formE)
+        {
+            var conn = new DBConnect(strConn);
+            try
+            {
+                SqlParameter[] p = new SqlParameter[3];
+                p[0] = new SqlParameter("@idForm", formE.formID);
+                p[1] = new SqlParameter("@formName",formE.formName);
+                p[2] = new SqlParameter("@UserName", formE.userName);
+                conn.ExecuteNonQueryParmarter("spInsertForm", p);
+                return true;
+            }
+            catch (Exception ex)
+            {
+                return false;
+                throw ex;
+            }
+            finally
+            {
+                conn.CloseConnection();
+            }
+        }
+
+        //Update 1
+        //xóa một loại form- cần chú ý kiểm tra xóa các bản ghi khóa ngoại ở các bảng Image, DetailForm,
+        public bool fnDeleteForm(string strConn,string idForm)
+        {
+            var conn = new DBConnect(strConn);
+            try
+            {
+                SqlParameter[] p = new SqlParameter[1];
+                p[0] = new SqlParameter("@idForm", idForm);
+                conn.ExecuteNonQueryParmarter("spDeleteForm", p);
+                return true;
+            }
+            catch (Exception ex)
+            {
+                return false;
+                throw ex;
+            }
+            finally
+            {
+                conn.CloseConnection();
+            }
+        }
+    }
+}
