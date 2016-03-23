266decff151ccbf278de8dc2dd0f987d2c7786c4
HangVy <nguyenhangth10a@gmail.com>
2016-03-23 07:08:16 +0700

    add BLL

---
+++ DAHangAnh/BLL/DetailFormBLL.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DAHangAnh.DAL;
+using DAHangAnh.DAL.Entities;
+using System.Data;
+namespace DAHangAnh.BLL
+{
+    public class DetailFormBLL
+    {
+        public DataTable GetListDetailForm(string strConn)
+        {
+            var detailFormDAL = new DetailFormDAL();
+            return detailFormDAL.fnGetListDetailForm(strConn);
+        }
+
+        public bool InsertForm(string strConn, DetailFormEntity detailFormE)
+        {
+            var detailFormDAL = new DetailFormDAL();
+            return detailFormDAL.fnInsertDetailForm(strConn, detailFormE);
+        }
+    }
+}
